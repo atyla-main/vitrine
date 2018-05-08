ee4121a0c0e0bb3afdaa4afbbe96519ce114429b
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-08 23:09:52 +0200

    APW-2 adding i18n

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import './styles/index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import HomePage from './components/homepage';
+import './services/i18n';
 
 ReactDOM.render(<HomePage />, document.getElementById('root'));
 registerServiceWorker();
