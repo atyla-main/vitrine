3075e78c91090b3bd5df6672842739211bd7fea6
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-18 00:46:41 +0200

    APW-8 adding route engine

--- src/App.js
+++ src/App.js
@@ -2,35 +2,16 @@ import React, { Component } from 'react';
 import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 import Footer from './components/footer/Footer';
 import Newsletter from './components/newsletter/Newsletter';
+import AtylaRouter from './router/atyla-router';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
+        <BrowserRouter>
+          <AtylaRouter />
+        </BrowserRouter>
         <Newsletter />
         <Footer />
       </div>
