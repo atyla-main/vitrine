bf1d27b4dee5953c77886aab90ac7e4f5f9f94a5
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-18 01:27:26 +0200

    APW-4 login logout with route

--- src/router/atyla-router.js
+++ src/router/atyla-router.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { Route, Switch, NavLink } from 'react-router-dom';
+import { Route, Switch, NavLink, Redirect } from 'react-router-dom';
 import NotFound from '../components/not-found/not-found';
 import HomePage from '../components/homepage';
+import Login from '../components/login/Login';
+import Auth from '../services/Auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      Auth.isUserAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+const LogOut = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+      render={props => {
+        Auth.deauthenticateUser();
+        return (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    }
+  />
+);
 
 class AtylaRouter extends React.Component {
   constructor(props) {
@@ -23,12 +61,17 @@ class AtylaRouter extends React.Component {
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse className="atylaNavBar-navLinks">
-            <NavLink to="/" className="atylaNavBar-normalLink" activeClassName="atylaNavBar-activeLink" exact>Home</NavLink>
+            <NavLink to="/login" className="atylaNavBar-normalLink" activeClassName="atylaNavBar-activeLink" exact>Login</NavLink>
+            <NavLink to="/protected" className="atylaNavBar-normalLink" activeClassName="atylaNavBar-activeLink" exact>Protected</NavLink>
+            <NavLink to="/logout" className="atylaNavBar-normalLink" activeClassName="atylaNavBar-activeLink" exact>Logout</NavLink>
             <NavLink to="/" className="atylaNavBar-normalLink mod-last" activeClassName="atylaNavBar-activeLink mod-last" exact>Home</NavLink>
           </Navbar.Collapse>
         </Navbar>
         <Switch>
           <Route exact path='/' component={HomePage}/>
+          <Route exact path='/login' component={Login}/>
+          <PrivateRoute path="/protected" component={NotFound} />
+          <LogOut path="/logout" component={LogOut} />
           <Route component={NotFound}/>
         </Switch>
       </div>
