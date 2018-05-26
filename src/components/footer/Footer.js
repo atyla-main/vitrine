693121ea0ff0b595ad372581185b101f8ab3913b
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-26 15:09:17 +0200

    APW-10 adding react-select about us and how it works page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 class Footer extends React.Component {
-  render()  {
+  render() {
     return (
       <div className="footer">
         <Grid className="footer-container">
@@ -10,21 +10,32 @@ class Footer extends React.Component {
             <Col sm={4} md={4}>
               <p className="footer-elementTitle">Our mission</p>
               <p>
-                This is a bit of space where you expose
-                your vision and the purpose of your
-                enterprise. This is equivalent to the why
-                part of a good comunication content set.
+                This is a bit of space where you expose your vision and the
+                purpose of your enterprise. This is equivalent to the why part
+                of a good comunication content set.
               </p>
             </Col>
             <Col sm={4} md={4}>
               <p className="footer-elementTitle">Navigate</p>
               <p>
-                <a className="footer-link">- Homepage</a>
-                <a className="footer-link">- Explore ICO’s</a>
-                <a className="footer-link">- How it works</a>
-                <a className="footer-link">- About us</a>
-                <a className="footer-link">- Sign-in / Sign-up</a>
-                <a className="footer-link">- Add an ICO</a>
+                <a className="footer-link" href="/">
+                  - Homepage
+                </a>
+                <a className="footer-link" href="/icos">
+                  - Explore ICO’s
+                </a>
+                <a className="footer-link" href="/howitworks">
+                  - How it works
+                </a>
+                <a className="footer-link" href="/about">
+                  - About us
+                </a>
+                <a className="footer-link" href="/login">
+                  - Sign-in / Sign-up
+                </a>
+                <a className="footer-link" href="/icos/1">
+                  - Add an ICO
+                </a>
               </p>
             </Col>
             <Col sm={4} md={4}>
---
+++ src/components/how-it-works/How-it-works.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HowItWorks extends React.Component {
+  render() {
+    return (
+      <div style={{ marginTop: '50px', height: '100vh', textAlign: 'center' }}>
+        <h1>How it works in progress</h1>
+      </div>
+    );
+  }
+}
+
+export default HowItWorks;
