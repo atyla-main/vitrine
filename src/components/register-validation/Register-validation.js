a71d5735024c798f76a71dc5a34af7bc9255a7a9
maisonbleue <vincent.trastour@lecollectionist.com>
2018-05-28 20:44:48 +0200

    APW-12 sign up

---
+++ src/components/register-validation/Register-validation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RegisterValidation extends React.Component {
+  render() {
+    return (
+      <div style={{ marginTop: '50px', height: '100vh', textAlign: 'center' }}>
+        <p>An email has been sent to your email address</p>
+        <p>Please check your email</p>
+      </div>
+    );
+  }
+}
+
+export default RegisterValidation;
